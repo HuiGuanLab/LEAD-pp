a7a5a4540b926c93b2c96d3a3ef65c2acc3401e6
zzh30 <126661359+zzh30@users.noreply.github.com>
2026-01-19 09:27:58 +0800

    Update run_linear.sh

--- run_linear.sh
+++ run_linear.sh
@@ -1,18 +1,20 @@
-task=$1   
-dataset=$2 
-pretrained=$3 
-checkpoints_name=$4
-num_classes=$5 
-cuda_device=$6 
+task=$1
+pretrained=$2
+checkpoints_name=$3
+num_classes=$4
+cuda_device=$5
 
-
-if [ ! -d ./checkpoints_linear/${checkpoints_name} ];then
-    mkdir -p ./checkpoints_linear/${checkpoints_name}
+# create checkpoint dir
+if [ ! -d "./checkpoints_linear/${checkpoints_name}" ]; then
+    mkdir -p "./checkpoints_linear/${checkpoints_name}"
 fi
 
+# auto root from task
+root="./DDT/crop_dataset/${task}_crop"
+
 CUDA_VISIBLE_DEVICES=${cuda_device} python linear_probing.py \
-  -t ${task} \
-  --lr 30.  --batch-size 256 --epochs 100\
-  --root ${dataset} --num-classes ${num_classes} \
-  --resume ./checkpoints/${pretrained}/model_last.pth\
-  --checkpoints ./checkpoints_linear/${checkpoints_name}
+  --lr 30. --batch-size 256 --epochs 100 \
+  --crop-root "${root}" \
+  --num_classes ${num_classes} \
+  --resume "./checkpoints/${pretrained}/model_last.pth" \
+  --results_dir "./checkpoints_linear/${checkpoints_name}"
