30139fa9bc423edf7002a3c42c5248360c16411b
zzh30 <126661359+zzh30@users.noreply.github.com>
2026-07-02 09:57:05 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -14,9 +14,15 @@ fi
 
 CUDA_VISIBLE_DEVICES=${cuda_device} python main.py \
   -a clip \
-  --lr 0.03 \
+  --lr 0.02 \
   --batch-size 64 \
   --epochs 100 \
+  --wd 5e-4 \
+  --moco-t 0.27 \
+  --moco-m 0.999 \
+  --alpha 0.5 \
+  --beta 20 \
+  --temperature 0.02 \
   --root ${dataset} \
   --crop_root ./DDT/crop_dataset/${task}_crop \
   --results-dir ./checkpoints/${checkpoints_name} \
